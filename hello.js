8477dd937cf5e8343268a5266c31ed1773beedbb
veronikaslc <veronika-koltunov@gmail.com>
2014-10-24 23:37:50 -0400

    Step 3
    omg, cmon, srsly.. files?

--- hello.js
+++ hello.js
@@ -1,5 +1,4 @@
-var sum = 0;
-for (i=2; i<process.argv.length; i++){
-	sum += Number(process.argv[i]);
-}
-console.log(sum);
+var fs = require('fs');
+var buf = fs.readFileSync(process.argv[2]);
+var num  = buf.toString().split('\n').length - 1;
+console.log(num);
