6d325b2e0a9db4f5af1dc4dfac8c29e3fb3c6d55
veronikaslc <veronika.koltunov@gmail.com>
2014-10-26 23:00:42 -0400

    step 9

--- vk9.js
+++ vk9.js
@@ -7,21 +7,21 @@ var stream = {srt1:'', srt2:'', srt3:'', f1:false, f2:false, f3:false};
 
 http.get(url1, function callback(response){	
 	response.pipe(bl( function(err, data){	
-		if (err) console.error;	
-		stream.str1+=data.toString();
+		if (err)  console.error	
+		else stream.str1+=data.toString();
 	}))	
 	response.on('end', function(){
 		stream.f1=true;
 		console.log(stream.str1);		
 		if (stream.f2) console.log(stream.str2)
-		if (stream.f3) console.log(stream.str3)	
+		if (stream.f3&stream.f2) console.log(stream.str3)	
 	})
 });
 
 http.get(url2, function callback(response){	
 	response.pipe(bl( function(err, data){	
-		if (err) console.error;	
-		stream.str2+=data.toString();
+		if (err) console.error	
+		else stream.str2+=data.toString();
 	}))	
 	response.on('end', function(){
 		stream.f2=true;	
@@ -32,8 +32,8 @@ http.get(url2, function callback(response){
 
 http.get(url3, function callback(response){	
 	response.pipe(bl( function(err, data){	
-		if (err) console.error;	
-		stream.str2+=data.toString();
+		if (err) console.error	
+		else stream.str3+=data.toString();
 	}))	
 	response.on('end', function(){
 		stream.f3=true;	
